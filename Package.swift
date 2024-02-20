f0923ed4a7cf55623fb9a270608bd2f4b5807476
Alban Pellegrini <alban.pellegrini@lafabriquedigitowl.com>
2024-02-19 19:43:33 -0500

    inter

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SPQuizzLib",
-            targets: ["SPQuizzPackage"]),
+            targets: ["SPQuizzTarget"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -20,7 +20,7 @@ let package = Package(
         .target(
             name: "SPQuizzTarget"),
         .testTarget(
-            name: "SPQuizz2TestTarget",
+            name: "SPQuizzTestTarget",
             dependencies: ["SPQuizzTarget"]),
     ]
 )
