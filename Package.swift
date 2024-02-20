60d396f49b6325d48bac9e6c53f7c0716eff5d65
Alban Pellegrini <alban.pellegrini@lafabriquedigitowl.com>
2024-02-19 20:01:02 -0500

    Package.swift

--- Package.swift
+++ Package.swift
@@ -4,23 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SPQuizzPackage",
-    platforms: [
-        .iOS(.v14),
-    ],
+    name: "YOLO",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Définissez les produits que votre package va fournir.
         .library(
-            name: "SPQuizzLib",
-            targets: ["SPQuizzTarget"]),
+            name: "YOLO",
+            targets: ["YOLO"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        // Définissez les cibles de build de votre package.
         .target(
-            name: "SPQuizzTarget"),
+            name: "YOLO",
+            dependencies: []),
         .testTarget(
-            name: "SPQuizzTestTarget",
-            dependencies: ["SPQuizzTarget"]),
+            name: "MySwiftPackageTests",
+            dependencies: ["YOLO"]),
     ]
 )
